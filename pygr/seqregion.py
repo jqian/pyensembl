c48796f2402c31862dceda588cc48ce0f2ca2b9a
qing <qing@Jennys_Computer.(none)>
2008-06-13 00:10:19 -0700

    Merge branch 'master' into jenny_pygr_ensembl
    Conflicts:
    
    	pygr/seqregion.py

--- pygr/seqregion.py
+++ pygr/seqregion.py
@@ -10,9 +10,7 @@ class SeqRegionInv(object):
            prefix = self.inverseDB.prefixDict[coord_system_id]
        except KeyError:
            prefix = self.inverseDB.prefixDefault
-       if prefix is None:
-           return k.id
-       name = k.id[len(prefix):] # remove the prefix from the identifier
+       name = k.id[len(prefix):] # remove the prefix from the true identifier
        sr = self.inverseDB.seqRegionDB.select('where coord_system_id=%s and name=%s',
                                               (coord_system_id,name)).next()
        return sr.id # its seq_region_id
@@ -30,8 +28,6 @@ class SeqRegion(dict):
     prefixDict: dictionary of the form {coord_system_id:prefix to add
                                                         to sr.name}
     prefixDefault: default prefix to add to sr.name
-    NB: a prefix of "" means sr.name will be used unchanged.
-    NB: a prefix of None means k.id will be used unchanged, ignoring sr.name.
         '''
         self.seqRegionDB = seqRegionDB
         self.coordSystems = coordSystems
@@ -57,13 +53,9 @@ class SeqRegion(dict):
         except KeyError:
             raise KeyError('unknown coordinate system %d' % sr.coord_system_id)
         try: # Ensembl doesn't store correct seqID, so add right prefix
-            prefix = self.prefixDict[sr.coord_system_id]
+            seqID = self.prefixDict[sr.coord_system_id] + sr.name
         except KeyError:
-            prefix = self.prefixDefault
-        if prefix is None:
-            seqID = k
-        else:
-            seqID = prefix + sr.name
+            seqID = self.prefixDefault + sr.name
         s = genome[seqID] # get the actual sequence object
         dict.__setitem__(self, k, s) # save in cache
         return s
@@ -116,6 +108,7 @@ class EnsemblMapper(object):
             l.append(aslice)
         return l
 
+<<<<<<< HEAD:pygr/seqregion.py
 from pygr import seqdb
 class EnsemblDNA(seqdb.DNASQLSequence):
     'interpret row objects as sequence object a la Ensembl dna table'
@@ -182,6 +175,8 @@ class AssemblyMapper(object):
         return u
     def __invert__(self):
         return self.inverseDB
+=======
+>>>>>>> master:pygr/seqregion.py
 
 
 
@@ -191,13 +186,9 @@ if __name__ == '__main__': # example code
     cursor = conn.cursor()
     seq_region = sqlgraph.SQLTable('homo_sapiens_core_47_36i.seq_region',
                                    cursor) 
-    dna = sqlgraph.SQLTable('homo_sapiens_core_47_36i.dna', cursor, 
-                            itemClass=EnsemblDNA,
-                            itemSliceClass=seqdb.SeqDBSlice,
-                            attrAlias=dict(seq='sequence'))
     import pygr.Data
     hg18 = pygr.Data.Bio.Seq.Genome.HUMAN.hg18() # human genome
-    srdb = SeqRegion(seq_region, {17:hg18, 4:dna}, {17:'chr',4:None})
+    srdb = SeqRegion(seq_region, {17:hg18})
     chr1 = srdb[226034]
     print len(chr1) # 247249719
     exonSliceDB = sqlgraph.SQLTable('homo_sapiens_core_47_36i.exon',
@@ -208,31 +199,16 @@ if __name__ == '__main__': # example code
     annoDB = AnnotationDB(exonSliceDB, srdb, sliceAttrDict=
                           dict(id='seq_region_id',stop='seq_region_end',
                                orientation='seq_region_strand'))
-    e = annoDB[73777]
+    e = annoDB[73777] # exon_id = 73777
     print e.phase, e.end_phase # 1 0
     s = e.sequence
     print str(s) #'GCAAGCTGTGGACAAGAAGTATGAAGGTCGCTTACAGCATTCTACACAAATTAGGCACAAAGCAGGAACCCATGGTCCGGCCTGGAGATAGG'
     print str(s.before()[-10:]) # 'GTATTCATAG' last 2 nt is splice-site
     print str(s.after()[:10]) # 'GTAAGTGCAA' first 2 nt is splice-site
-    mapper = EnsemblMapper(annoDB, srdb) # find exons for any sequence slice
+    mapper = EnsemblMapper(annoDB, srdb) # 
     ival = chr1[:100000]
     print mapper[ival] # find exons in this interval
     print 'neg:', mapper[-ival] # forces annotations to opposite ori...
-    s = dna[143909] # get this sequence object
-    print len(s) # 41877
-    print str(s[:10]) # CACCCTGCCC
-    amap = AssemblyMapper(srdb, 4, 17) # test the assembly mapper
-    contig = srdb[149878] # get a contig
-    c = contig[:10] # a short slice of its beginning
-    u = amap[c] # map forwards it to hg18
-    print str(c) # 'ACCCCTTACC'
-    print str(u) # 'accccttacc'
-    print c.orientation # 1
-    print u.orientation # -1
-    ival = chr1[1000000:1000010] # get an hg18 interval
-    c = (~amap)[ival] # map back to contig interval
-    print repr(ival), repr(c) # chr1[1000000:1000010] 158512[13848:13858]
-    print str(ival), str(c) # ACGTGGCTGC ACGTGGCTGC
 
     #conn.close()
 
