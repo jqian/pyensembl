3010f5c58104ad04bb895bd3938d53775bb57200
qing <qing@Jennys_Computer.(none)>
2008-08-11 18:20:35 -0700

    added PtranscriptToPexon mapping

--- ensembl/featuremapping.py
+++ ensembl/featuremapping.py
@@ -43,3 +43,44 @@ class TranscriptToExon(object):
 
     def __invert__(self):
         return self.inverseDB
+
+
+class PtranscriptToPexonInv(object):
+    'inverse of a PtranscriptToPexon mapping: an Ensembl prediction_exon obj -> an Ensembl prediction_transcript obj'
+
+    def __init__(self, mapper):
+        self.inverseDB = mapper
+    
+    def __getitem__(self, k):
+        'find the corresponding transcript to the given exon'
+
+        ptranscriptID = k.prediction_transcript_id
+        ptranscript = self.inverseDB.ptranscriptDB[ptranscriptID] 
+        return ptranscript
+
+    def __invert__(self):
+        return self.inverseDB
+
+class PtranscriptToPexon(object):
+    'Provide a mapping of a prediction_transcript obj -> a set of prediction_exon objects'
+
+    def __init__(self, ptranscriptDB, pexonDB):
+        self.pexonDB = pexonDB
+        self.ptranscriptDB = ptranscriptDB
+        self.inverseDB = PtranscriptToPexonInv(self)
+        #self.cursor = self.ptranscriptDB.cursor 
+
+    def __getitem__(self, k):
+        ptranscriptID = k.id
+        #n = self.cursor.execute('select exon_id from %s where transcript_id = %s' %(self.exon_transcript, transcriptID))
+        t = self.pexonDB.select('where prediction_transcript_id = %s', (ptranscriptID))
+        #t = self.cursor.fetchall()
+        pexons = []
+        for row in t:
+            #id = row[0]
+            #exon = self.exonDB[id]
+            pexons.append(row)
+        return pexons
+
+    def __invert__(self):
+        return self.inverseDB
